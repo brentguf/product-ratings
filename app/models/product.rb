3a69c1869a9b3a34a71c27d00bbaa68c64e21d59
brentguf <brent.guffens@gmail.com>
2021-09-25 12:18:19 +0200

    Average rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ApplicationRecord
   has_many :product_reviews, dependent: :destroy
 
   validates :title, presence: true
+
+  def average_rating
+    ratings = product_reviews.to_a
+      .map { |product_review| product_review.rating }
+
+    (ratings.sum.to_f / ratings.size).round(1)
+  end
 end
