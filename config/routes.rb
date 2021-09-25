fdeddef2e09a2d98531747d7c0b433f232494172
brentguf <brent.guffens@gmail.com>
2021-09-25 11:22:53 +0200

    Nest product_reviews routes in products routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'products#index'
   
-  resources :products
-  resources :product_reviews
-
+  resources :products do 
+    resources :product_reviews
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
