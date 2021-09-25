d802d03c47d0d52cb87197ba3aa51b8e29f20edc
brentguf <brent.guffens@gmail.com>
2021-09-25 12:06:24 +0200

    ProductReview model validations

--- app/models/product_review.rb
+++ app/models/product_review.rb
@@ -1,3 +1,6 @@
 class ProductReview < ApplicationRecord
   belongs_to :product
+
+  validates_numericality_of :rating, greater_than_or_equal_to: 0, less_than_or_equal_to: 5
+  validates :description, presence: true
 end
