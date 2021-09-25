9724fd017558908201ada45a08e60d83e2d9e4a4
brentguf <brent.guffens@gmail.com>
2021-09-25 11:49:28 +0200

    Only new/create product_reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'products#index'
   
   resources :products do 
-    resources :product_reviews
+    resources :product_reviews, only: [:new, :create]
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
