dab58cecd1fbdbe110bdea97cc4185c6f04b2328
brentguf <brent.guffens@gmail.com>
2021-09-25 15:03:48 +0200

    No route restrictions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'products#index'
   
   resources :products do 
-    resources :product_reviews, only: [:new, :create]
+    resources :product_reviews
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
